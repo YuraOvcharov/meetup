51b3250916548e88378b832c4bd81b87460b4cce
YuraOvcharov <yuraovcharov13@gmail.com>
2018-10-08 14:08:49 +0300

    preloader

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,35 @@ $(document).ready(function(e) {
 		e.preventDefault();
 		modalContacts.removeClass('is-visible');
 	});
+
+
+	
+	let 
+		images = document.images,
+		images_total_count = images.length,
+		images_loaded_count = 0,
+		preloader = document.getElementById('page-preloader'),
+		perc_display = document.getElementById('load-perc');
+
+
+	for (let i = 0; i < images_total_count; i++){
+		image_clone = new Image();
+		image_clone.onload = image_loaded;
+		image_clone.onerror = image_loaded;
+		image_clone.src = images[i].src;
+	}
+
+	function image_loaded(){
+		images_loaded_count++;
+		perc_display.innerHTML = (( (100 / images_total_count) * images_loaded_count) << 0) + '%';//<<0 побитовая операция, чтобы было видно только целую часть
+		if (images_loaded_count >= images_total_count){
+			setTimeout(function(){
+				if (!preloader.classList.contains('done')){
+					preloader.classList.add('done');
+				}
+			},1000);
+		}
+	};
 });
 
 
